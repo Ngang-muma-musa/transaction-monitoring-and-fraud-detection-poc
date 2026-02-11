31ef61734a3e93d33cee2b9fb242ffbae3242500
Ngang-muma-musa <ngangmuma00@gmail.com>
2026-02-11 18:37:44 +0100

    update drop query

--- migrations/000001_create-transaction-table.up.sql
+++ migrations/000001_create-transaction-table.up.sql
@@ -1,10 +1,5 @@
--- 1. Remove Indexes first
 DROP INDEX IF EXISTS idx_transactions_user_velocity;
 
--- 2. Drop Tables
 DROP TABLE IF EXISTS transactions;
 
--- 3. Drop Custom Types (Enums)
--- Note: You cannot drop a type if it is still being used by a column, 
--- which is why we drop the table first.
 DROP TYPE IF EXISTS transaction_status;
